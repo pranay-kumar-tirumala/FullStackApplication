e29262a4947dcc0391a68aa4cda6d33d707185a2
Pranay Kumar Tirumala <pranaykumar562@gmail.com>
2023-03-11 23:27:18 +0530

    Implemented ReviewController, ReviewService & ReviewRepository

---
+++ src/main/java/com/pranaykumar/movies/Repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.pranaykumar.movies.Repository;
+
+import com.pranaykumar.movies.Model.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
